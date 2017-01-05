fa2b885572c34377bbac39226ba7e73e2138c638
Sahishnu <bhoolku5443@gmail.com>
2017-01-04 19:48:08 -0500

    change food location on space bar

--- js/sketch.js
+++ js/sketch.js
@@ -91,6 +91,8 @@ function keyPressed(){
 				snake.dir(-1,0);
 		}else if(keyCode == RIGHT_ARROW || keyCode == 68){
 				snake.dir(1,0);
+		}else if(keyCode == 32){
+			food.pickLocation();
 		}
 	}
 }
